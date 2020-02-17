9069e7daf1098b0fb72080a1ebe9459b4a5cd724
jeremy8250 <jeremy8250@gmail.com>
2020-02-17 17:21:59 +0800

    1. 优化代码注释 2. 优化find方法，定位符支持元组和非元组

--- test_selenium/page/base_page.py
+++ test_selenium/page/base_page.py
@@ -1,5 +1,3 @@
-from time import sleep
-
 from selenium import webdriver
 from selenium.webdriver.remote.webdriver import WebDriver
 
@@ -9,30 +7,42 @@ class BasePage:
 
     _base_url = ""
     # 初始化_base_url为空，这样不用每个页面都需要加_base_url
+    # 类变量
+
     _driver = None
-    # _driver为内部变量，只有初始化的时候声明为None，后面才能被正确调用
+    # _driver:隐藏driver,让外部不可调用
+    # _driver为内部变量，只有当初始化的时候声明为None，后面才能被正确调用
+
     def __init__(self, driver: WebDriver = None, reuse=False):
         # reuse:是否复用已有的浏览器进程
         if driver is None:
+        # 如果没有chromedriver,就按照下面的方法给一个
             if reuse:
                 options = webdriver.ChromeOptions()
                 options.debugger_address = "127.0.0.1:9222"
                 self._driver = webdriver.Chrome(options=options)
+                # 如果page页reuse=true，则复用当前的chrome浏览器
             else:
-                # _driver:隐藏driver,让外部不可调用
-                # index页面会使用这个
                 self._driver = webdriver.Chrome()
-            self._driver.implicitly_wait(3)
+                # 如果page页reuse=false，则起一个新的浏览器进程
         else:
-            # login与register的页面需要用这个方法
             self._driver = driver
+            # login与register的页面需要用这个方法
 
         if self._base_url != "":
             self._driver.get(self._base_url)
             # 允许每个类初始化的时候打开自己定义的地址
+            # page页中的_base_url会覆盖basepage中的_base_url,使这段代码生效
 
-    def find(self, locator):
-        return self._driver.find_element(*locator)
+        self._driver.implicitly_wait(3)
+        # 隐式等待
+
+    def find(self, by, locator=""):
+        if isinstance(by, tuple):
+            return self._driver.find_element(*by)
+            # *by作用:解析page页中定义的元组类型的定位符
+        else:
+            return self._driver.find_element(by, locator)
 
     def close(self):
         self._driver.quit()
